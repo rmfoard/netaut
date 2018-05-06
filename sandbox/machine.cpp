1e8e3561a48b6d6357ab7cb1e7662345c9830b8f
Richard Foard <rfoard@jhu.edu>
2018-05-06 15:05:05 -0400

    Use uintmax_t and guard overflow

--- sandbox/machine.cpp
+++ sandbox/machine.cpp
@@ -1,28 +1,31 @@
 //#include "Snap.h"
 #include <assert.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <vector>
 
-int Raise(const int base, const int exponent) {
-    int result = 1;
+uintmax_t Raise(const int base, const int exponent) {
+    uintmax_t result = 1;
     for (int i = 0; i < exponent; i += 1) result *= base;
 
     return result;
 }
 
-int RuleNr(const int nrStates, const int nrActions, const std::vector<int> ruleParts) {
-    int ruleNr = 0;
+uintmax_t RuleNr(const int nrStates, const int nrActions, const std::vector<int> ruleParts) {
+    uintmax_t ruleNr = 0;
     for (int partNr = 0; partNr < nrStates; partNr += 1) {
         assert(ruleParts[partNr] < nrActions);
+        assert(ruleParts[partNr] * Raise(nrActions,partNr) <= (UINTMAX_MAX - ruleNr));
         ruleNr += ruleParts[partNr] * Raise(nrActions, partNr);
     }
 
     return ruleNr;
 }
 
-std::vector<int>* RuleParts(const int nrStates, const int nrActions, const int ruleNr) {
+std::vector<int>* RuleParts(const int nrStates, const int nrActions, const uintmax_t ruleNr) {
     std::vector<int>* ruleParts = new std::vector<int>(nrStates);
-    for (int state = 0, residue = ruleNr; state < nrStates; state += 1) {
+    uintmax_t residue = ruleNr;
+    for (int state = 0; state < nrStates; state += 1) {
         (*ruleParts)[state] = residue % nrActions;
         residue /= nrActions;
         assert(state < nrStates - 1 || residue == 0);
@@ -34,10 +37,10 @@ std::vector<int>* RuleParts(const int nrStates, const int nrActions, const int r
 int main(const int argc, const char* argv[]) {
     std::vector<int> ruleParts = { 1, 3, 5, 7, 0, 11, 9, 2  };
 
-    int ruleNr = RuleNr(8, 20, ruleParts);
-    printf("ruleNr: %d\n", ruleNr);
+    uintmax_t ruleNr = RuleNr(8, 20, ruleParts);
+    printf("ruleNr: %lld\n", ruleNr);
 
     std::vector<int>* rulePartsBack = RuleParts(8, 20, ruleNr);
     for (int i = 0; i < 8; i += 1)
-        printf("rule %d: action %d\n", i, (*rulePartsBack)[i]);
+        printf("rulePart %d: action %d\n", i, (*rulePartsBack)[i]);
 }
