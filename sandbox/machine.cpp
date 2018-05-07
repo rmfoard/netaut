483298096b4e7227606bdb54d9e0257536c602cb
Richard Foard <rfoard@jhu.edu>
2018-05-07 07:02:02 -0400

    Change from '*NEANet','net' -> '*NEGraph','graph'

--- sandbox/machine.cpp
+++ sandbox/machine.cpp
@@ -8,47 +8,47 @@
 uintmax_t RuleNr(const int, const int, const std::vector<int>);
 std::vector<int>* RuleParts(const int, const int, const uintmax_t);
 
-// TODO: Consider moving this to a 'net_factory' module.
+// TODO: Consider moving this to a 'graph_factory' module.
 template <class TGraph>
-static TPt<TGraph> RingNet(int nrNodes) {
+static TPt<TGraph> RingGraph(int nrNodes) {
     assert(nrNodes > 1);
-    TPt<TGraph> net = TGraph::New();
+    TPt<TGraph> graph = TGraph::New();
 
-    for (int n = 0; n < nrNodes; n += 1) net->AddNode(n);
-    for (int n = 0; n < nrNodes - 1; n += 1) net->AddEdge(n, n + 1);
-    net->AddEdge(nrNodes - 1, 0);
-    net->AddEdge(0, nrNodes - 1);
-    for (int n = 1; n < nrNodes; n += 1) net->AddEdge(n, n - 1);
+    for (int n = 0; n < nrNodes; n += 1) graph->AddNode(n);
+    for (int n = 0; n < nrNodes - 1; n += 1) graph->AddEdge(n, n + 1);
+    graph->AddEdge(nrNodes - 1, 0);
+    graph->AddEdge(0, nrNodes - 1);
+    for (int n = 1; n < nrNodes; n += 1) graph->AddEdge(n, n - 1);
 
-    return net;
+    return graph;
 }
 
 class Machine {
 
 public:
-    Machine(uintmax_t, int, PNEANet);
+    Machine(uintmax_t, int, PNEGraph);
     void Cycle();
 
 private:
     uintmax_t m_ruleNr;
     int m_nrNodes;
-    PNEANet m_net;
+    PNEGraph m_graph;
     std::vector<int>* m_ruleParts;
 };
 
-Machine::Machine(uintmax_t ruleNr, int nrNodes, PNEANet net) {
+Machine::Machine(uintmax_t ruleNr, int nrNodes, PNEGraph graph) {
     m_ruleNr = ruleNr;
     m_nrNodes = nrNodes;
-    m_net = net;
+    m_graph = graph;
     m_ruleParts = RuleParts(8, 20, ruleNr);
 }
 
 // Cycle: Run one step of the loaded rule.
 void Machine::Cycle() {
-    PNEANet nextNet = TNEANet::New();
+    PNEGraph nextGraph = TNEGraph::New();
 
-    // Traverse the current net building 'nextNet'.
-    for (TNEANet::TNodeI NI = m_net->BegNI(); NI < m_net->EndNI(); NI++) ;
+    // Traverse the current graph building 'nextGraph'.
+    for (TNEGraph::TNodeI NI = m_graph->BegNI(); NI < m_graph->EndNI(); NI++) ;
 }
 
 int main(const int argc, const char* argv[]) {
@@ -56,12 +56,12 @@ int main(const int argc, const char* argv[]) {
     uintmax_t ruleNr = RuleNr(8, 20, ruleParts);
     printf("ruleNr: %lld\n", ruleNr);
 
-    // build a ring network
-    PNEANet net = RingNet<TNEANet>(10);
+    // build a ring graph
+    PNEGraph graph = RingGraph<TNEGraph>(10);
 
     std::vector<int>* rulePartsBack = RuleParts(8, 20, ruleNr);
     for (int i = 0; i < 8; i += 1)
         printf("rulePart %d: action %d\n", i, (*rulePartsBack)[i]);
 
-    Machine* m = new Machine(ruleNr, 10, net);
+    Machine* m = new Machine(ruleNr, 10, graph);
 }
