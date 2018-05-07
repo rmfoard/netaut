4674b4a8c4ea4d5659a0afcf881d718f1cc3fd3a
Richard Foard <rfoard@jhu.edu>
2018-05-07 07:30:23 -0400

    Move graph creation into [renamed] MachineS constructor

--- sandbox/machine.cpp
+++ sandbox/machine.cpp
@@ -9,42 +9,41 @@ uintmax_t RuleNr(const int, const int, const std::vector<int>);
 std::vector<int>* RuleParts(const int, const int, const uintmax_t);
 
 // TODO: Consider moving this to a 'graph_factory' module.
-template <class TGraph>
-static TPt<TGraph> RingGraph(int nrNodes) {
+static void BuildRing(int nrNodes, PNEGraph graph) {
     assert(nrNodes > 1);
-    TPt<TGraph> graph = TGraph::New();
 
     for (int n = 0; n < nrNodes; n += 1) graph->AddNode(n);
     for (int n = 0; n < nrNodes - 1; n += 1) graph->AddEdge(n, n + 1);
     graph->AddEdge(nrNodes - 1, 0);
     graph->AddEdge(0, nrNodes - 1);
     for (int n = 1; n < nrNodes; n += 1) graph->AddEdge(n, n - 1);
-
-    return graph;
 }
 
-class Machine {
+class MachineS {
 
 public:
-    Machine(uintmax_t, int, PNEGraph);
+    MachineS(uintmax_t, int);
     void Cycle();
 
 private:
     uintmax_t m_ruleNr;
     int m_nrNodes;
     PNEGraph m_graph;
+    int *m_color;
     std::vector<int>* m_ruleParts;
 };
 
-Machine::Machine(uintmax_t ruleNr, int nrNodes, PNEGraph graph) {
+MachineS::MachineS(uintmax_t ruleNr, int nrNodes) {
     m_ruleNr = ruleNr;
     m_nrNodes = nrNodes;
-    m_graph = graph;
+    m_graph = TNEGraph::New();
+    BuildRing(nrNodes, m_graph);
+    m_color = new int[nrNodes];
     m_ruleParts = RuleParts(8, 20, ruleNr);
 }
 
 // Cycle: Run one step of the loaded rule.
-void Machine::Cycle() {
+void MachineS::Cycle() {
     PNEGraph nextGraph = TNEGraph::New();
 
     // Traverse the current graph building 'nextGraph'.
@@ -56,12 +55,9 @@ int main(const int argc, const char* argv[]) {
     uintmax_t ruleNr = RuleNr(8, 20, ruleParts);
     printf("ruleNr: %lld\n", ruleNr);
 
-    // build a ring graph
-    PNEGraph graph = RingGraph<TNEGraph>(10);
-
     std::vector<int>* rulePartsBack = RuleParts(8, 20, ruleNr);
     for (int i = 0; i < 8; i += 1)
         printf("rulePart %d: action %d\n", i, (*rulePartsBack)[i]);
 
-    Machine* m = new Machine(ruleNr, 10, graph);
+    MachineS* m = new MachineS(ruleNr, 10);
 }
