3e527689a2de8369fec6e4b4e7fe49f55697c83a
Richard Foard <rfoard@jhu.edu>
2018-05-11 18:27:38 -0400

    Frame new rule scheme

--- sandbox/rules.cpp
+++ sandbox/rules.cpp
@@ -1,38 +1,23 @@
 #include <assert.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string>
 #include <vector>
 #include "rules.h"
 
-//---------------
-// Action names (indexed by action number)
-//---------------
-const std::string Rules::actionNames[] = {
-    "white",
-    "black",
-    "none",
-    "invert",
+const std::string Rules::topo_action_component_names[] = {
+    "L",
+    "LL",
+    "LR",
+    "R",
+    "RL",
+    "RR"
+};
 
-    "r_rr+white",
-    "r_rr+black",
-    "r_rr",
-    "r_rr+invert",
-
-    "l_ll+white",
-    "l_ll+black",
-    "l_ll",
-    "l_ll+invert",
-
-    "r_rl+white",
-    "r_rl+black",
-    "r_rl",
-    "r_rl+invert",
-
-    "l_lr+white",
-    "l_lr+black",
-    "l_lr",
-    "l_lr+invert"
+const std::string Rules::node_action_names[] = {
+    "WHITE",
+    "BLACK"
 };
 
 //---------------
@@ -54,7 +39,7 @@ long long unsigned Rules::Raise(const int base, const int exponent) {
 // The rule number is a radix 'nrActions' number in which the nth digit is the
 // action number applicable to nodes in state 'n'.
 //---------------
-long long unsigned Rules::RuleNr(const int nrStates, const int nrActions, const std::vector<int> ruleParts) {
+long long unsigned Rules::RuleNr(const int nrStates, const int nrActions, int* ruleParts) {
     assert(nrStates > 1);
     assert(nrActions > 0);
 
@@ -63,6 +48,7 @@ long long unsigned Rules::RuleNr(const int nrStates, const int nrActions, const 
     for (int partNr = 0; partNr < nrStates; partNr += 1) {
         increase = ruleParts[partNr] * Raise(nrActions, partNr);
         assert(ruleParts[partNr] < nrActions);
+        // TODO: Replace UINTMAX_MAX, below.
         assert(increase <= (UINTMAX_MAX - ruleNr));
         ruleNr += increase;
     }
@@ -77,17 +63,36 @@ long long unsigned Rules::RuleNr(const int nrStates, const int nrActions, const 
 // machine, returns an integer vector in which the nth entry is the action number
 // applicable to nodes in state 'n'.
 //---------------
-std::vector<int>* Rules::RuleParts(const int nrStates, const int nrActions, const long long unsigned ruleNr) {
+int* Rules::RuleParts(const int nrStates, const int nrActions, const long long unsigned ruleNr) {
     assert(nrStates > 1);
     assert(nrActions > 0);
 
-    std::vector<int>* ruleParts = new std::vector<int>(nrStates);
+    int* ruleParts = new int[NR_TRIAD_STATES];
     long long unsigned residue = ruleNr;
+
     for (int state = 0; state < nrStates; state += 1) {
-        (*ruleParts)[state] = residue % nrActions;
+        *(ruleParts + state) = residue % nrActions;
         residue /= nrActions;
         assert(state < nrStates - 1 || residue == 0);
     }
 
     return ruleParts;
+}
+
+//---------------
+std::string Rules::RulePartText(const int rulePart) {
+    return "L<" + topo_action_component_names[(rulePart/2) / 6]
+      + "+"
+      + "R<" + topo_action_component_names[(rulePart/2) % 6]
+      + "+"
+      + "N<" + node_action_names[rulePart % 2];
+}
+
+//---------------
+int main(int argc, char* argv[]) {
+    Rules* r = new Rules();
+    int* ruleParts = r->RuleParts(NR_TRIAD_STATES, NR_ACTIONS, (unsigned long long) 10658);
+    for (int i = 0; i < NR_TRIAD_STATES; i += 1)
+        printf("[%d] ___ %d = %s\n", i, ruleParts[i], r->RulePartText(ruleParts[i]).c_str());
+    printf("finis.\n");
 }
--- sandbox/rules.h
+++ sandbox/rules.h
@@ -5,32 +5,42 @@
 class Rules {
 
 public:
-    long long unsigned RuleNr(const int, const int, const std::vector<int>);
-    std::vector<int>* RuleParts(const int, const int, const long long unsigned);
+    long long unsigned RuleNr(const int, const int, int*);
+    int* RuleParts(const int, const int, const long long unsigned);
+    std::string RulePartText(const int);
 
-    static const std::string actionNames[];
+    static const std::string topo_action_component_names[];
+    static const std::string node_action_names[];
 
 private:
     long long unsigned Raise(const int, const int);
 };
 
 //---------------
-// Action code = graph state op * 4 + node state op
-//
-// Node state operations
+// TODO: Explain the following.
 //---------------
+
+// Node states, node action components
 #define NWHITE 0
 #define NBLACK 1
-#define NNONE 2
-#define NINVERT 3
+#define NR_NODE_STATES 2
 
-//---------------
-// Graph state operations
-//---------------
-#define GNONE 0
-#define GR_RR 1 // replace redge with redge[redge]
-#define GL_LL 2 // replace ledge with ledge[ledge]
-#define GR_RL 3 // replace redge with redge[ledge]
-#define GL_LR 4 // replace ledge with ledge[redge]
-//---------------
+// Graph topology action components
+#define LEDGE 0
+#define LLEDGE 1
+#define LREDGE 2
+#define REDGE 3
+#define RLEDGE 4
+#define RREDGE 5
+#define NR_TOPO_ACTION_COMPONENTS 6
+
+#define NR_TRIAD_STATES (NR_NODE_STATES * NR_NODE_STATES * NR_NODE_STATES)
+#define NR_ACTIONS (NR_TOPO_ACTION_COMPONENTS * NR_TOPO_ACTION_COMPONENTS * NR_NODE_STATES)
+
+// Rule part (action) identifier assembly macro
+#define RP(LACTION, RACTION, NACTION) (((LACTION*6) + RACTION) * 2 + NACTION)
+
+// A rule number is a radix NR_TRIAD_STATES number, each "digit" of which is:
+//  ((<replacement for left edge> * NR_TOPO_ACTION_COMPONENTS) + <replacement for right edge>) * 2 +
+//    <replacement for node>
 #endif
