67894d5b9f51ebf4a0e4101116644fc8043af75c
Richard Foard <rfoard@jhu.edu>
2018-05-15 00:47:05 -0400

    Draft overhauled 'rule' class

---
+++ sandbox/rule.cpp
@@ -0,0 +1,102 @@
+#include <assert.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <string>
+#include "rule.h"
+
+Rule::Rule(rulenr_t ruleNr) {
+    static_assert(sizeof(rulenr_t) == sizeof(uintmax_t), "faulty type size assumption");
+    m_ruleNr = ruleNr;
+}
+
+Rule::Rule(const int* ruleParts) {
+    rulenr_t ruleNr = 0;
+    rulenr_t increase;
+    for (int partNr = 0; partNr < NR_TRIAD_STATES; partNr += 1) {
+        increase = ruleParts[partNr] * Raise(NR_ACTIONS, partNr);
+        assert(ruleParts[partNr] < NR_ACTIONS);
+        assert(increase <= (RULENR_MAX - ruleNr));
+        ruleNr += increase;
+    }
+    m_ruleNr = ruleNr;
+}
+
+Rule::Rule(const char* ruleText) {
+}
+
+//---------------
+// get_ruleNr
+//---------------
+rulenr_t Rule::get_ruleNr() { return m_ruleNr; }
+
+//---------------
+// get_ruleParts
+//---------------
+const int* Rule::get_ruleParts() {
+    int* rp = new int[NR_TRIAD_STATES];
+    rulenr_t dividend = m_ruleNr;
+    for (int triadState = 0; triadState < NR_TRIAD_STATES; triadState += 1) {
+        rp[triadState] = dividend % NR_ACTIONS;
+        dividend /= NR_ACTIONS;
+    }
+    return rp;
+}
+
+
+//---------------
+// get_ruleText
+//---------------
+const char* Rule::get_ruleText() {
+    std::string rt = std::string("");
+    const int* rp = get_ruleParts();
+    for (int partNr = 0; partNr < NR_TRIAD_STATES; partNr += 1) {
+        rt += RulePartText(rp[partNr]);
+        if (partNr < NR_TRIAD_STATES - 1) rt += "; ";
+    }
+    return rt.c_str();
+}
+
+//---------------
+// Raise
+//
+// Performs integer exponentiation.
+//---------------
+long long unsigned Rule::Raise(const int base, const int exponent) {
+    long long unsigned result = 1;
+    for (int i = 0; i < exponent; i += 1) result *= base;
+    return result;
+}
+
+//---------------
+// RulePartText
+//
+// Returns a rule part in text form.
+//---------------
+const std::string Rule::RulePartText(const int rulePart) {
+    return std::string("L,R,N");
+}
+
+//---------------
+int main(const int argc, const char* argv[]) {
+    Rule* r = new Rule((rulenr_t) 1234);
+    printf("rule: %llu\n", r->get_ruleNr());
+    delete r;
+
+    const int ruleParts[] = { 71, 71, 71, 71, 71, 71, 71, 71 };
+    r = new Rule(ruleParts);
+    printf("rule: %llu\n", r->get_ruleNr());
+    const int* rp = r->get_ruleParts();
+    for (int i = 0; i < NR_TRIAD_STATES; i += 1) printf("part %d: %d\n", i, rp[i]);
+    delete rp;
+    printf("rule text: %s\n", r->get_ruleText());
+    delete r;
+
+    const int ruleParts2[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    r = new Rule(ruleParts2);
+    printf("rule: %llu\n", r->get_ruleNr());
+    const int* rp2 = r->get_ruleParts();
+    for (int i = 0; i < NR_TRIAD_STATES; i += 1) printf("part %d: %d\n", i, rp2[i]);
+    printf("rule text: %s\n", r->get_ruleText());
+    delete rp2;
+    delete r;
+}
---
+++ sandbox/rule.h
@@ -0,0 +1,78 @@
+#ifndef RULES_H
+#define RULES_H
+#include <inttypes.h>
+#include <string>
+
+// Node states, node action components
+#define NWHITE 0
+#define NBLACK 1
+#define NR_NODE_STATES 2
+
+// Graph topology action components
+#define LEDGE 0
+#define LLEDGE 1
+#define LREDGE 2
+#define REDGE 3
+#define RLEDGE 4
+#define RREDGE 5
+#define NR_POSS_DSTS 6
+
+#define NR_TRIAD_STATES (NR_NODE_STATES * NR_NODE_STATES * NR_NODE_STATES)
+#define NR_ACTIONS (NR_POSS_DSTS * NR_POSS_DSTS * NR_NODE_STATES)
+
+typedef long long unsigned rulenr_t;
+
+// The implicit assumption underlying the following is that
+// sizeof(rulenr_t) == sizeof(uintmax_t). The assumption is
+// checked in a static assertion in the 'Rule(rulenr_t)' constructor.
+#define RULENR_MAX UINTMAX_MAX
+
+//---------------
+// Rule Object
+//
+// For each possible set of node states in a left neighbor/node/right neighbor
+// triad, a rule encodes the action to be applied to yield the next
+// left neighbor/node state/right neighbor configuration, i.e., it specifies
+// replacement values for the node's left out-edge, state, and right out-edge.
+// By convention, the first out-edge is the "left," the second the "right."
+//
+// Rules may specify that an edge be replaced by the node's left out-edge,
+// its right out-edge, its left neighbor's left or right out-edges, or its
+// right neighbor's left or right out-edges. These new edge destinations are denoted
+// by prefixes L, R, RL, RR, LL, and LR, respectively.
+//
+// A rule is represented internally as a "rule number."
+// A rule number is a radix NR_TRIAD_STATES number, each "digit" of which is
+// a "rule part" value. The digit in the i-th power position gives the rule part
+// to be applied for triads whose left neighbor/node/right neighbor node states
+// equal i when interpreted as a binary number.
+//
+// A rule part value is the packed representation:
+//
+//  ((<new edge dst for left edge> * NR_POSS_DSTS) + <new edge dst for right edge>) * 2 +
+//    <new state for node>
+//
+// in which the "new edge dst" codes are as defined above, under "Graph topology action
+// components," and the "new state for node" codes are as defined under "Node states,i
+// node action components."
+//---------------
+
+class Rule {
+
+public:
+    // Constructors, from "rule number," "rule parts," and "rule text."
+    Rule(const long long unsigned);
+    Rule(const int*);
+    Rule(const char*);
+
+    rulenr_t get_ruleNr();
+    const int* get_ruleParts();
+    const char* get_ruleText();
+
+private:
+    long long unsigned Raise(const int, const int);
+    const std::string RulePartText(const int);
+
+    rulenr_t m_ruleNr;
+};
+#endif
