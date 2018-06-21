893843f69eb1ffe2c0a9ed18e7d3f9474d7d6814
Richard Foard <rfoard@jhu.edu>
2018-06-20 22:08:59 -0400

    WIP toward overhauled randrule

--- sandbox/randrule.cpp
+++ sandbox/randrule.cpp
@@ -18,8 +18,10 @@
 //---------------
 struct CommandOpts {
     int randSeed;
+    int reserve;
     std::string acceptFile;
     std::string rejectFile;
+    std::string cacheFile;
 };
 
 static CommandOpts cmdOpt;
@@ -31,8 +33,10 @@ void ParseCommand(const int argc, char* argv[]) {
 
     // Set options to default values.
     cmdOpt.randSeed = -1;
-    cmdOpt.acceptFile = std::string("");
-    cmdOpt.rejectFile = std::string("");
+    cmdOpt.reserve = 0;
+    cmdOpt.cacheFile = "";
+    cmdOpt.acceptFile = "";
+    cmdOpt.rejectFile = "";
 
     static struct option long_options[] = {
         // Boolean options, e.g.,
@@ -42,6 +46,10 @@ void ParseCommand(const int argc, char* argv[]) {
         {"accept", required_argument, 0, 'a'},
         {"reject", required_argument, 0, 'r'},
         {"randseed", required_argument, 0, 's'},
+        {"reserve", required_argument, 0, 'e'},
+        {"cache", required_argument, 0, 'c'},
+        {"reset", no_argument, 0, 't'},
+        {"help", no_argument, 0, 'h'},
         {0, 0, 0, 0}
     };
 
@@ -70,9 +78,30 @@ void ParseCommand(const int argc, char* argv[]) {
             cmdOpt.rejectFile = std::string(optarg);
             break;
 
+          case 'c':
+            cmdOpt.cacheFile = std::string(optarg);
+            break;
+
           case 's':
             cmdOpt.randSeed = atoi(optarg);
             break;
+
+          case 'e':
+            cmdOpt.reserve = atoi(optarg);
+            break;
+
+          case 't':
+            // eradicate the cache
+            break;
+
+          case 'h':
+            printf("Command options:\n");
+            for (auto entry : long_options) if (entry.name != NULL) {
+                printf("  --%s", entry.name);
+                if (entry.has_arg) printf(" <value>");
+                printf("\n");
+            }
+            exit(0);
 
           case '?':
             errorFound = true;
@@ -132,10 +161,28 @@ int main(const int argc, char* argv[]) {
 
     ParseCommand(argc, argv);
 
+    // Process filter specification files if they're present.
     if (cmdOpt.acceptFile != "")
         ; //printf("process acceptFile: %s\n", cmdOpt.acceptFile.c_str());
     if (cmdOpt.rejectFile != std::string(""))
         ; //printf("process rejectFile: %s\n", cmdOpt.rejectFile.c_str());
+
+    // Generate and draw from a cache file if specified.
+    if (cmdOpt.cacheFile != "") { // if a cache file is specified
+        // if it does not exist
+            // create and fill it, set position to the beginning
+        // endif
+        // read the next position
+        // if it's not at the end
+            // read the next entry
+            // advance and record the position
+            // return the next entry
+            // exit(0)
+        // else
+            // show a warning (and fall to generate a "singleton")
+        // endif
+    }
+    
     std::cout << GenRandRule();
     exit(0);
 }
