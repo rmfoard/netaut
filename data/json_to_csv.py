0adb3f66bd1d00efb65850e80cb3a5494f9e82a5
Richard Foard <rfoard@jhu.edu>
2018-06-24 20:41:49 -0400

    Extend json_to_csv to write all input

--- data/json_to_csv.py
+++ data/json_to_csv.py
@@ -4,12 +4,13 @@ import sys
 
 
 def main():
-    if len(sys.argv) != 3:
-        print "usage: python json_to_csv.py <jsonfile> <csvfile>"
+    if len(sys.argv) != 2:
+        print "usage: python json_to_csv.py <json file base name (w/o '.json')>"
         sys.exit(1)
+    basename = sys.argv[1]
 
-    # List of fields to be included in the csv file (in order)
-    fieldnames = [
+    # List of fields to be included in the main csv file (in order)
+    main_names = [
         'allowSelfEdges',
         'avgClustCoef',
         'cycleCheckDepth',
@@ -37,15 +38,67 @@ def main():
         'version'
     ]
 
-    # Pass the json file writing csv rows.
-    with open(sys.argv[1], 'r') as jsonfile:
-        with open(sys.argv[2], 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction='ignore')
-            writer.writeheader()
-            for line in jsonfile:
-                dct = json.loads(line)
-                writer.writerow(dct)
+    cc_names = [
+        'runId',
+        'ccSize',
+        'ccCount'
+    ]
+    cc_dct = {}
 
+    id_names = [
+        'runId',
+        'inDegree',
+        'inDegreeCount'
+    ]
+    id_dct = {}
+
+    tr_names = [
+        'runId',
+        'triad',
+        'triadCount'
+    ]
+    tr_dct = {}
+
+    # Pass the json file writing the four derived csv's.
+    with open(basename + '.json', 'r') as jsonfile:
+        with open(basename + '.csv', 'w') as main_f:
+            main_writer = csv.DictWriter(main_f, fieldnames=main_names, extrasaction='ignore')
+            main_writer.writeheader()
+
+            with open(basename + '_cc.csv', 'w') as cc_f:
+                cc_writer = csv.DictWriter(cc_f, fieldnames=cc_names, extrasaction='ignore')
+                cc_writer.writeheader()
+
+                with open(basename + '_id.csv', 'w') as id_f:
+                    id_writer = csv.DictWriter(id_f, fieldnames=id_names, extrasaction='ignore')
+                    id_writer.writeheader()
+
+                    with open(basename + '_tr.csv', 'w') as tr_f:
+                        tr_writer = csv.DictWriter(tr_f, fieldnames=tr_names, extrasaction='ignore')
+                        tr_writer.writeheader()
+
+                        for line in jsonfile:
+                            main_dct = json.loads(line)
+                            run_id = main_dct['runId']
+                            main_writer.writerow(main_dct)
+
+                            cc_dct['runId'] = run_id
+                            for pair in main_dct['ccSizeCount']:
+                                cc_dct['ccSize'] = pair[0]
+                                cc_dct['ccCount'] = pair[1]
+                                cc_writer.writerow(cc_dct)
+
+                            id_dct['runId'] = run_id
+                            for pair in main_dct['inDegreeCount']:
+                                id_dct['inDegree'] = pair[0]
+                                id_dct['inDegreeCount'] = pair[1]
+                                id_writer.writerow(id_dct)
+
+                            tr_dct['runId'] = run_id
+                            for triad in xrange(6):
+                                tr_dct['triad'] = triad
+                                tr_dct['triadCount'] = main_dct['triadOccurrences'][triad]
+                                tr_writer.writerow(tr_dct)
 
 if __name__ == '__main__':
     main()
