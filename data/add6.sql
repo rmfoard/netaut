f40b309354ac9dc8aad51543588665a86e5286be
Richard Foard <rfoard@jhu.edu>
2018-08-02 11:42:26 -0400

    Add entropy columns +2 others

---
+++ data/add6.sql
@@ -0,0 +1,6 @@
+alter table runs add column initInDegreeEntropy double precision;
+alter table runs add column initOutDegreeEntropy double precision;
+alter table runs add column finInDegreeEntropy double precision;
+alter table runs add column finOutDegreeEntropy double precision;
+alter table runs add column nrOutDegrees integer;
+alter table runs add column finNrNodes integer;
