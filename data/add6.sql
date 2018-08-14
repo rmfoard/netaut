76d505c0a94f34f6c9da0ad4c8d0e95d775809b0
Richard Foard <rfoard@jhu.edu>
2018-08-14 11:16:11 -0400

    Add R setup script

--- data/add6.sql
+++
@@ -1,6 +0,0 @@
-alter table runs add column initInDegreeEntropy double precision;
-alter table runs add column initOutDegreeEntropy double precision;
-alter table runs add column finInDegreeEntropy double precision;
-alter table runs add column finOutDegreeEntropy double precision;
-alter table runs add column nrOutDegrees integer;
-alter table runs add column finNrNodes integer;
