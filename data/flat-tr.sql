cff7ce2c7747f5077893415f68f9cfa9f6fc1da2
Richard Foard <rfoard@jhu.edu>
2018-08-17 07:48:15 -0400

    Correct flattener

--- data/flat-tr.sql
+++ data/flat-tr.sql
@@ -1,10 +1,10 @@
 select
-    (sum(triadcount) from paredtr where triad = 0) as tr0,
-    (sum(triadcount) from paredtr where triad = 1) as tr1,
-    (sum(triadcount) from paredtr where triad = 2) as tr2,
-    (sum(triadcount) from paredtr where triad = 3) as tr3,
-    (sum(triadcount) from paredtr where triad = 4) as tr4,
-    (sum(triadcount) from paredtr where triad = 5) as tr5,
-    (sum(triadcount) from paredtr where triad = 6) as tr6,
-    (sum(triadcount) from paredtr where triad = 7) as tr7
+    (select sum(triadcount) from paredtr where triad = 0) as tr0,
+    (select sum(triadcount) from paredtr where triad = 1) as tr1,
+    (select sum(triadcount) from paredtr where triad = 2) as tr2,
+    (select sum(triadcount) from paredtr where triad = 3) as tr3,
+    (select sum(triadcount) from paredtr where triad = 4) as tr4,
+    (select sum(triadcount) from paredtr where triad = 5) as tr5,
+    (select sum(triadcount) from paredtr where triad = 6) as tr6,
+    (select sum(triadcount) from paredtr where triad = 7) as tr7
 into flattr;
