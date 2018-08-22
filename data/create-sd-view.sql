89420e31fd933b8be09d7d6eb5d55a13b552d4c2
Richard Foard <rfoard@jhu.edu>
2018-08-22 10:10:59 -0400

    Improve sd view creation script

--- data/create-sd-view.sql
+++ data/create-sd-view.sql
@@ -1,1 +1,1 @@
-create view sd as select s.*, d.avgclustcoef, d.diameter, d.indegreeentropy, d.iterationnr, d. nrccsizes, d.nrccs, d.nrclosedtriads, d.nrindegrees, d.nrnodes as finnrnodes, d.nropentriads from s, d where s.runid = d.runid;
+create view sd as select s.*, d.avgclustcoef, d.diameter, d.effdiameter90pctl, d.indegreeentropy, d.iterationnr, d.nrccsizes, d.nrccs, d.nrclosedtriads, d.nrindegrees, d.nrnodes, d.nropentriads, d.nroutdegrees, d.outdegreeentropy from s, d where s.runid = d.runid;
