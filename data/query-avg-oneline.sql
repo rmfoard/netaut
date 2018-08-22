f693843427b7c76bfbca3617a325019063798176
Richard Foard <rfoard@jhu.edu>
2018-08-22 07:39:52 -0400

    Create R 'source' file from sql script

--- data/query-avg-oneline.sql
+++
@@ -1,1 +0,0 @@
-select initnrnodes, avg(avgClustCoef) as aavgClustCoef, avg(diameter) as adiameter, avg(inDegreeEntropy) as ainDegreeEntropy, avg(nrCcSizes) as anrCcSizes, avg(nrCcs) as anrCcs, avg(nrClosedTriads) as  anrClosedTriads, avg(nrInDegrees) as anrInDegrees, avg(finNrNodes) as afinNrNodes, avg(nrOpenTriads) as anrOpenTriads from sd where iterationNr <> 0 group by initnrnodes;
