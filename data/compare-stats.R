410c33a6e3f5fa2ca49dc68d945abb2cdaa166f9
Richard Foard <rfoard@jhu.edu>
2018-08-25 09:55:20 -0400

    Improve compare-stats.R

--- data/compare-stats.R
+++ data/compare-stats.R
@@ -1,2 +1,4 @@
-p <- ggplot(data=avgstats) + geom_point(mapping=aes(x=factor(graphsize), y=avgclustcoef, color=machine, shape=collapsing))
+p <- ggplot(data=avgstats) +
+    geom_point(mapping=aes(x=factor(graphsize), y=iterations, color=machine, shape=collapsing), size=4) +
+    scale_shape_manual(values=c(1, 13, 2))
 print(p)
