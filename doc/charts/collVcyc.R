8a11bd0933ab2f92716c329543f49df77b98eddc
Richard Foard <rfoard@jhu.edu>
2018-09-02 11:09:59 -0400

    Refine collVcyc plot as exemplar

--- doc/charts/collVcyc.R
+++ doc/charts/collVcyc.R
@@ -1,15 +1,27 @@
 # Graph collapse vs. cycle outcomes for various initial graph sizes, machine C
 # TODO: Show proportions instead of absolute counts.
-# TODO: Add a descriptive title and improve axis labels
 print("(for the model_c database, open as 'con')")
 dbGetQuery(con, "select initnrnodes, cyclelength, '' as outcome into tmp from s where machinetype = 'C'")
 dbGetQuery(con, "update tmp set outcome = 'collapse' where cyclelength = -1")
 dbGetQuery(con, "update tmp set outcome = 'cycle' where cyclelength <> -1")
 tmp <- dbGetQuery(con, "select * from tmp")
 dbGetQuery(con, "drop table tmp")
-p <- ggplot(data=tmp) + geom_bar(mapping=aes(x=factor(initnrnodes), fill=outcome))
+p <- ggplot(data=tmp) +
+    geom_bar(
+        mapping=aes(x=factor(initnrnodes), fill=outcome),
+        position="dodge"
+    ) +
+    theme(
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)
+    ) +
+    labs(
+        title=paste("Collapses outnumber cycles."),
+        subtitle=paste("[Machine C]"),
+        x="Initial graph size",
+        y="Number of runs"
+    )
 rm(tmp)
-dev.copy(png, "collVcyc.png")
-dev.off()
 print(p)
-rm(p)
+dev.copy(png, filename="collVcyc.png")
+dev.off()
