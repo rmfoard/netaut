910e166879ab077606476a538baa2938d2b811a8
Richard Foard <rfoard@jhu.edu>
2018-09-25 19:31:46 -0400

    WIP article

--- doc/article/setup-crm.R
+++ doc/article/setup-crm.R
@@ -1,5 +1,5 @@
 library("RPostgreSQL")
 library("tidyverse")
 pg = dbDriver("PostgreSQL")
-con = dbConnect(pg, user="richard", port=5432, dbname="model_d")
+con = dbConnect(pg, user="richard", port=5432, dbname="crm")
 print("Remember to set the search_path to ... if necessary.")
