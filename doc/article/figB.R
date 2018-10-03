28392f4c324cfdc8e0d8637d311136428ac8c8a6
Richard Foard <rfoard@jhu.edu>
2018-10-02 23:18:58 -0400

    Revised figB.R

--- doc/article/figB.R
+++ doc/article/figB.R
@@ -1,7 +1,7 @@
 # Number of in-degrees, C vs R machines, all initial graph sizes
 # (depends on the 'sdavgs' table [avg-sd.sql])
 #
-p <- ggplot(data=sdavgs) +
+p <- ggplot(data=filter(sdavgs, initnrnodes <= 1024, machinetype == 'C' | machinetype == 'R')) +
     geom_line(
         mapping=aes(x=initnrnodes, y=anrindegrees, color=machinetype),
         size=2
@@ -10,7 +10,7 @@ p <- ggplot(data=sdavgs) +
         mapping=aes(x=initnrnodes, y=anrindegrees, color=machinetype),
         size=2
     ) +
-    scale_color_manual(values=c("blue", "red")) +
+    scale_color_manual(values=c("blue", "red", "green", "gray")) +
     theme(
         plot.title = element_text(hjust = 0.5, size=16),
         plot.subtitle = element_text(hjust = 0.5),
@@ -20,7 +20,7 @@ p <- ggplot(data=sdavgs) +
         title=paste("Average # of in-degrees, C and R machines"),
         subtitle=paste("[B]"),
         x="Initial graph size (nodes)",
-        y="Run length (iterations)"
+        y="Number of in-degrees"
     ) +
     labs(color="Machine")
 print(p)
