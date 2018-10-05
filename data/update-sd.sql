cd5e02afd5528c26b4715ebc82ec8da5d2f39d8e
Richard Foard <rfoard@jhu.edu>
2018-10-05 17:21:36 -0400

    Correct update-sd.sql

--- data/update-sd.sql
+++ data/update-sd.sql
@@ -2,10 +2,5 @@ select s.runid, sum(cccount * ccsize) into finnrnodes from s, c where s.runid = 
 alter table finnrnodes rename column sum to nrnodes;
 create index finnrnodes_idx_runid on finnrnodes(runid);
 create index sd_idx_runid on sd(runid);
-update sd set nrnodes = finnrnodes.nrnodes from finnrnodes where sd.runid = finnrnodes.runid;
+update sd set finnrnodes = finnrnodes.nrnodes from finnrnodes where sd.runid = finnrnodes.runid;
 drop table finnrnodes;
---
-select runid, max(indegreesize) into tmpmid from i group by runid;
-alter table sd add column maxindegree integer;
-update sd set maxindegree = max from tmpmid where sd.runid = tmpmid.runid;
-drop table tmpmid;
