1baf1bc93857c689e31892bd4be3db1a5a38d1ca
Richard Foard <rfoard@jhu.edu>
2018-10-06 07:59:33 -0400

    Go multi-legend in outcomes.R

--- doc/article/outcomes.R
+++ doc/article/outcomes.R
@@ -1,18 +1,15 @@
 p <- ggplot() +
-    geom_line(data=filter(sdavgs, machinetype == 'C', outcome == 'Y', initnrnodes < 2048),
-      mapping=aes(x=initnrnodes, y=anrindegrees),
-      color="blue"
+    geom_line(data=filter(
+        sdavgs,
+        mach_outc == 'C->COL' | mach_outc == 'C->CYC' | mach_outc == 'R->COL' | mach_outc == 'CM->CYC',
+        initnrnodes < 2048
+      ),
+      mapping=aes(
+        x=initnrnodes,
+        y=anrindegrees,
+        color=machinetype,
+        linetype=outcome
+      )
     ) +
-    geom_line(data=filter(sdavgs, machinetype == 'C', outcome == 'L', initnrnodes < 2048),
-      mapping=aes(x=initnrnodes, y=anrindegrees),
-      color="brown"
-    ) +
-    geom_line(data=filter(sdavgs, machinetype == 'CM', outcome == 'Y', initnrnodes < 2048),
-      mapping=aes(x=initnrnodes, y=anrindegrees),
-      color="green"
-    ) +
-    geom_line(data=filter(sdavgs, machinetype == 'R', outcome == 'L', initnrnodes < 2048),
-      mapping=aes(x=initnrnodes, y=anrindegrees),
-      color="red"
-    )
+    scale_color_manual(values=c("blue", "gray50", "red"))
 print(p)
