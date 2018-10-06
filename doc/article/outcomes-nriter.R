431bcfbc4b95cfade191b21617965e2eeca8e3d9
Richard Foard <rfoard@jhu.edu>
2018-10-06 08:09:31 -0400

    Add 'outcomes.R' variants

---
+++ doc/article/outcomes-nriter.R
@@ -0,0 +1,15 @@
+p <- ggplot() +
+    geom_line(data=filter(
+        sdavgs,
+        mach_outc == 'C->COL' | mach_outc == 'C->CYC' | mach_outc == 'R->COL' | mach_outc == 'CM->CYC',
+        initnrnodes < 2048
+      ),
+      mapping=aes(
+        x=initnrnodes,
+        y=anriterations,
+        color=machinetype,
+        linetype=outcome
+      )
+    ) +
+    scale_color_manual(values=c("blue", "gray50", "red"))
+print(p)
