b40a851ea741048078aee0d838a29d48cd8ae20b
Richard Foard <rfoard@jhu.edu>
2019-01-14 14:33:06 -0500

    Correct previous commit

--- data/create-ruleparts-tbl.sql
+++ data/create-ruleparts-tbl.sql
@@ -4,12 +4,12 @@
 ---------------
 CREATE TABLE ruleparts (
     rulenr      bigint,
-    rp7,        integer,
-    rp6,        integer,
-    rp5,        integer,
-    rp4,        integer,
-    rp3,        integer,
-    rp2,        integer,
-    rp1,        integer,
-    rp0,        integer
+    rp7        integer,
+    rp6        integer,
+    rp5        integer,
+    rp4        integer,
+    rp3        integer,
+    rp2        integer,
+    rp1        integer,
+    rp0        integer
 );
