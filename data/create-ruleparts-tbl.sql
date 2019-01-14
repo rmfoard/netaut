126efe7fcbd7739795b4eda0e8190efa0d440e07
Richard Foard <rfoard@jhu.edu>
2019-01-14 14:28:24 -0500

    Add create-ruleparts-tbl.sql'

---
+++ data/create-ruleparts-tbl.sql
@@ -0,0 +1,15 @@
+---------------
+-- ruleparts table: relates rulenr to its 8 rule-parts
+--
+---------------
+CREATE TABLE ruleparts (
+    rulenr      bigint,
+    rp7,        integer,
+    rp6,        integer,
+    rp5,        integer,
+    rp4,        integer,
+    rp3,        integer,
+    rp2,        integer,
+    rp1,        integer,
+    rp0,        integer
+);
