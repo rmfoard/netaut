e263daeff0bd604fe0fdeaa8a7a6c01e21249a7c
Richard Foard <rfoard@jhu.edu>
2019-02-15 18:02:12 -0500

    Stub 'runner' class files

---
+++ code/runner.cpp
@@ -0,0 +1,34 @@
+#include <assert.h>
+//#include <getopt.h>
+//#include <inttypes.h>
+#include <stdio.h>
+//#include <stdint.h>
+//#include <algorithm>
+//#include <ctime>
+//#include <chrono>
+//#include <fstream>
+//#include <iostream>
+//#include <limits>
+#include <string>
+//#include <vector>
+//#include <json/json.h>
+//#include "rule.h"
+//#include "machine.h"
+//#include "machine2D.h"
+//#include "machineR.h"
+#include "runner.h"
+
+//===============
+// class Runner methods
+//===============
+
+//---------------
+// Runner constructor
+//
+//---------------
+Runner::Runner(int placeholder) {
+}
+
+//---------------
+void Runner::Placeholder(int placeholder) {
+}
---
+++ code/runner.h
@@ -0,0 +1,14 @@
+#ifndef RUNNER_H
+#define RUNNER_H
+
+//---------------
+class Runner {
+
+public:
+    Runner(int);
+
+private:
+    void Placeholder(int);
+
+};
+#endif
