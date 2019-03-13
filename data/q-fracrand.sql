a4b13b138a4b9a0c1bcc94012c8e7d8876af2c6a
Richard Foard <rfoard@jhu.edu>
2019-03-13 16:04:26 -0400

    Correct ranges in 'q-fracrand.sql'

--- data/q-fracrand.sql
+++ data/q-fracrand.sql
@@ -12,15 +12,15 @@ insert into fracrand (statname, statmin, statmax, fracrand) values ('diameter', 
 insert into fracrand (statname, statmin, statmax, fracrand) values ('diameter', 128, 192, (select (select count(*) from sd where initnrnodes = 257 and 128 <= diameter and diameter <= 192) * 1.0 / (select count from n)));
 insert into fracrand (statname, statmin, statmax, fracrand) values ('diameter', 192, 256, (select (select count(*) from sd where initnrnodes = 257 and 192 <= diameter and diameter <= 256) * 1.0 / (select count from n)));
 --
-insert into fracrand (statname, statmin, statmax, fracrand) values ('nrCcSizes', 1, 64, (select (select count(*) from sd where initnrnodes = 257 and 1 <= nrCcSizes and nrCcSizes <= 64) * 1.0 / (select count from n)));
-insert into fracrand (statname, statmin, statmax, fracrand) values ('nrCcSizes', 64, 128, (select (select count(*) from sd where initnrnodes = 257 and 64 <= nrCcSizes and nrCcSizes <= 128) * 1.0 / (select count from n)));
-insert into fracrand (statname, statmin, statmax, fracrand) values ('nrCcSizes', 128, 192, (select (select count(*) from sd where initnrnodes = 257 and 128 <= nrCcSizes and nrCcSizes <= 192) * 1.0 / (select count from n)));
-insert into fracrand (statname, statmin, statmax, fracrand) values ('nrCcSizes', 192, 256, (select (select count(*) from sd where initnrnodes = 257 and 192 <= nrCcSizes and nrCcSizes <= 256) * 1.0 / (select count from n)));
+insert into fracrand (statname, statmin, statmax, fracrand) values ('nrCcSizes', 1, 6, (select (select count(*) from sd where initnrnodes = 257 and 1 <= nrCcSizes and nrCcSizes <= 6) * 1.0 / (select count from n)));
+insert into fracrand (statname, statmin, statmax, fracrand) values ('nrCcSizes', 6, 12, (select (select count(*) from sd where initnrnodes = 257 and 6 <= nrCcSizes and nrCcSizes <= 12) * 1.0 / (select count from n)));
+insert into fracrand (statname, statmin, statmax, fracrand) values ('nrCcSizes', 12, 18, (select (select count(*) from sd where initnrnodes = 257 and 12 <= nrCcSizes and nrCcSizes <= 18) * 1.0 / (select count from n)));
+insert into fracrand (statname, statmin, statmax, fracrand) values ('nrCcSizes', 18, 24, (select (select count(*) from sd where initnrnodes = 257 and 18 <= nrCcSizes and nrCcSizes <= 24) * 1.0 / (select count from n)));
 --
-insert into fracrand (statname, statmin, statmax, fracrand) values ('nrInDegrees', 1, 64, (select (select count(*) from sd where initnrnodes = 257 and 1 <= nrInDegrees and nrInDegrees <= 64) * 1.0 / (select count from n)));
-insert into fracrand (statname, statmin, statmax, fracrand) values ('nrInDegrees', 64, 128, (select (select count(*) from sd where initnrnodes = 257 and 64 <= nrInDegrees and nrInDegrees <= 128) * 1.0 / (select count from n)));
-insert into fracrand (statname, statmin, statmax, fracrand) values ('nrInDegrees', 128, 192, (select (select count(*) from sd where initnrnodes = 257 and 128 <= nrInDegrees and nrInDegrees <= 192) * 1.0 / (select count from n)));
-insert into fracrand (statname, statmin, statmax, fracrand) values ('nrInDegrees', 192, 256, (select (select count(*) from sd where initnrnodes = 257 and 192 <= nrInDegrees and nrInDegrees <= 256) * 1.0 / (select count from n)));
+insert into fracrand (statname, statmin, statmax, fracrand) values ('nrInDegrees', 1, 6, (select (select count(*) from sd where initnrnodes = 257 and 1 <= nrInDegrees and nrInDegrees <= 6) * 1.0 / (select count from n)));
+insert into fracrand (statname, statmin, statmax, fracrand) values ('nrInDegrees', 6, 12, (select (select count(*) from sd where initnrnodes = 257 and 6 <= nrInDegrees and nrInDegrees <= 12) * 1.0 / (select count from n)));
+insert into fracrand (statname, statmin, statmax, fracrand) values ('nrInDegrees', 12, 18, (select (select count(*) from sd where initnrnodes = 257 and 12 <= nrInDegrees and nrInDegrees <= 18) * 1.0 / (select count from n)));
+insert into fracrand (statname, statmin, statmax, fracrand) values ('nrInDegrees', 18, 24, (select (select count(*) from sd where initnrnodes = 257 and 18 <= nrInDegrees and nrInDegrees <= 24) * 1.0 / (select count from n)));
 --
 insert into fracrand (statname, statmin, statmax, fracrand) values ('nrCcs', 1, 64, (select (select count(*) from sd where initnrnodes = 257 and 1 <= nrCcs and nrCcs <= 64) * 1.0 / (select count from n)));
 insert into fracrand (statname, statmin, statmax, fracrand) values ('nrCcs', 64, 128, (select (select count(*) from sd where initnrnodes = 257 and 64 <= nrCcs and nrCcs <= 128) * 1.0 / (select count from n)));
