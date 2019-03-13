07475263faf44578b55e1628b971cbaa2beb899b
Richard Foard <rfoard@jhu.edu>
2019-03-13 15:28:44 -0400

    Correct q-fracsearch.sql

--- data/q-fracsearch.sql
+++ data/q-fracsearch.sql
@@ -1,19 +1,19 @@
 select statname, statmin, statmax, sum(nrfitrules)*1.0 / sum(nrdistinctrules) as fracsearch, cumfitnessexp
-into p-acc from gs where statname = 'avgClustCoef' and cumfitnessexp = 0.6
+into p_acc from gs where statname = 'avgClustCoef' and cumfitnessexp = 0.6
 group by statmin, statmax order by statmin, statmax;
 --
 select statname, statmin, statmax, sum(nrfitrules)*1.0 / sum(nrdistinctrules) as fracsearch, cumfitnessexp
-into p-d from gs where statname = 'diameter' and cumfitnessexp = 1.4
+into p_d from gs where statname = 'diameter' and cumfitnessexp = 1.4
 group by statmin, statmax order by statmin, statmax;
 --
 select statname, statmin, statmax, sum(nrfitrules)*1.0 / sum(nrdistinctrules) as fracsearch, cumfitnessexp
-into p-ncc from gs where statname = 'nrCcs' and cumfitnessexp = 1.0
+into p_ncc from gs where statname = 'nrCcs' and cumfitnessexp = 1.0
 group by statmin, statmax order by statmin, statmax;
 --
 select statname, statmin, statmax, sum(nrfitrules)*1.0 / sum(nrdistinctrules) as fracsearch, cumfitnessexp
-into p-nccs from gs where statname = 'nrCcSizes' and cumfitnessexp = 1.4
+into p_nccs from gs where statname = 'nrCcSizes' and cumfitnessexp = 1.4
 group by statmin, statmax order by statmin, statmax;
 --
 select statname, statmin, statmax, sum(nrfitrules)*1.0 / sum(nrdistinctrules) as fracsearch, cumfitnessexp
-into p-ni from gs where statname = 'nrInDegrees' and cumfitnessexp = 1.0
+into p_ni from gs where statname = 'nrInDegrees' and cumfitnessexp = 1.0
 group by statmin, statmax order by statmin, statmax;
